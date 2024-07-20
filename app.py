132eda36c56be89bbe7e50b3878fa04d0c191154
amoynahan89 <amfinsov@gmail.com>
2024-07-19 19:16:40 -0700

    About to make major design change

--- app.py
+++ app.py
@@ -12,21 +12,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """
-    response = requests.get("https://mempool.space/api/v1/prices")
+
+    response = requests.get("https://mempool.space/api/v1/historical-price")
 
     if response.status_code == 200:
-        raw_price = response.json()
-        price = raw_price["USD"]
-    return render_template("index.html", price=price)
-    """
+        data = response.json()
+
+    times = [result["time"] for result in data["prices"]]
+    prices = [result["USD"] for result in data["prices"]]
 
     fig = Figure(figsize=(5, 4), dpi=100)
-    
 
     # Do some plotting
     ax = fig.add_subplot()
-    ax.plot([1, 2, 3, 4], [0, 10, 20, 40])
+    ax.plot(times, prices)
 
     # Option 1: Save the figure to a file
     image_path = os.path.join(app.root_path, "static/images/test.png")
@@ -38,10 +37,6 @@ def index():
 def intro():
     return render_template("intro.html")
 
-@app.route('/get_started')
-def get_started():
-    return render_template("get_started.html")
-
 @app.route('/guide')
 def guide():
     return render_template("guide.html")
@@ -52,4 +47,23 @@ def technical():
 
 @app.route('/history')
 def history():
-    return render_template("history.html")
+
+    response = requests.get("https://mempool.space/api/v1/historical-price")
+
+    if response.status_code == 200:
+        data = response.json()
+
+    times = [result["time"] for result in data["prices"] if result["USD"] > 0]
+    prices = [result["USD"] for result in data["prices"] if result["USD"] > 0]
+
+    fig = Figure(figsize=(5, 2.5), dpi=500)
+
+    # Do some plotting
+    ax = fig.add_subplot()
+    ax.plot(times, prices)
+
+    # Option 1: Save the figure to a file
+    image_path = os.path.join(app.root_path, "static/images/chart.png")
+    fig.savefig(image_path)
+
+    return render_template("history.html", image_url="/static/images/chart.png")
--- tests.py
+++ tests.py
@@ -10,12 +10,35 @@ def usd(n):
 response = requests.get("https://mempool.space/api/v1/historical-price")
 data = response.json()
 
+times = [result["time"] for result in data["prices"] if result["USD"] == 0]
+prices = [result["USD"] for result in data["prices"] if result["USD"] == 0]
 
 
-times = [result["time"] for result in data["prices"]]
-prices = [result["USD"] for result in data["prices"]]
+
 
 fig, ax = plt.subplots()             # Create a figure containing a single Axes.
 ax.plot(times, prices)               # Plot some data on the Axes.
 plt.show()                           # Show the figure.
 
+
+"""
+
+{
+  prices: [
+    {
+      "time": 1499904000,
+      "EUR": 1964,
+      "USD": 2254.9
+    }
+  ],
+  exchangeRates: {
+    "USDEUR": 0.92,
+    "USDGBP": 0.78,
+    "USDCAD": 1.36,
+    "USDCHF": 0.89,
+    "USDAUD": 1.53,
+    "USDJPY": 149.48
+  }
+}
+
+"""
