7958122547247781408e0fdfe8edca9a4f8ea549
amoynahan89 <amfinsov@gmail.com>
2024-07-20 09:57:31 -0700

    Got new grid layout working

--- app.py
+++ app.py
@@ -11,59 +11,47 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index():    
+    # Chech if we have file
+    my_dir = os.getcwd()
+    with os.scandir(f"{my_dir}/static/images") as dir:
+        for file in dir:
+            if file.name.startswith('test') and file.is_file():
+                return render_template("index.html", image_url="/static/images/test.png")
+                
+        response = requests.get("https://mempool.space/api/v1/historical-price")
 
-    response = requests.get("https://mempool.space/api/v1/historical-price")
+        if response.status_code == 200:
+            data = response.json()
 
-    if response.status_code == 200:
-        data = response.json()
+        times = [result["time"] for result in data["prices"] if result["USD"] > 0]
+        prices = [result["USD"] for result in data["prices"] if result["USD"] > 0]
 
-    times = [result["time"] for result in data["prices"]]
-    prices = [result["USD"] for result in data["prices"]]
+        fig = Figure(figsize=(5, 4), dpi=100)
 
-    fig = Figure(figsize=(5, 4), dpi=100)
+        # Do some plotting
+        ax = fig.add_subplot()
+        ax.plot(times, prices)
 
-    # Do some plotting
-    ax = fig.add_subplot()
-    ax.plot(times, prices)
+        # Option 1: Save the figure to a file
+        image_path = os.path.join(app.root_path, "static/images/test.png")
+        fig.savefig(image_path)
 
-    # Option 1: Save the figure to a file
-    image_path = os.path.join(app.root_path, "static/images/test.png")
-    fig.savefig(image_path)
 
     return render_template("index.html", image_url="/static/images/test.png")
 
-@app.route('/intro')
-def intro():
-    return render_template("intro.html")
+@app.route('/how_to_use')
+def how_to_use():
+    return render_template("how_to_use.html")
 
-@app.route('/guide')
-def guide():
-    return render_template("guide.html")
+@app.route('/learn')
+def learn():
+    return render_template("learn.html")
 
-@app.route('/technical')
-def technical():
-    return render_template("technical.html")
+@app.route('/explore')
+def explore():
+    return render_template("explore.html")
 
-@app.route('/history')
-def history():
-
-    response = requests.get("https://mempool.space/api/v1/historical-price")
-
-    if response.status_code == 200:
-        data = response.json()
-
-    times = [result["time"] for result in data["prices"] if result["USD"] > 0]
-    prices = [result["USD"] for result in data["prices"] if result["USD"] > 0]
-
-    fig = Figure(figsize=(5, 2.5), dpi=500)
-
-    # Do some plotting
-    ax = fig.add_subplot()
-    ax.plot(times, prices)
-
-    # Option 1: Save the figure to a file
-    image_path = os.path.join(app.root_path, "static/images/chart.png")
-    fig.savefig(image_path)
-
-    return render_template("history.html", image_url="/static/images/chart.png")
+@app.route('/donate')
+def donate():
+    return render_template("donate.html")
--- tests.py
+++ tests.py
@@ -1,3 +1,16 @@
+import os
+
+my_dir = os.getcwd()
+
+with os.scandir(f"{my_dir}/static/images") as dir:
+    for file in dir:
+        if file.name.startswith('test') and file.is_file():
+            print(file.name)
+
+
+
+
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
@@ -21,7 +34,7 @@ ax.plot(times, prices)               # Plot some data on the Axes.
 plt.show()                           # Show the figure.
 
 
-"""
+
 
 {
   prices: [
